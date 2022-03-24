7fcf9b272f9f20c815f6445ccbc74548c1805fc6
Chris Mwalimo <82896090+codeschris@users.noreply.github.com>
2022-03-24 21:58:13 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#use random module after function is working
+from random import randint
+
+fridge = ["Rice with beans", "Ugali and sukuma"]
+
+#Storing your food inside the fridge
+def storeFridge():
+    respond = input("Have something to add into the fridge?(yes/no) ")
+    if respond == "yes":
+        newFood = input("Add something new: ")
+        fridge.append(newFood)
+    else:
+        print("Alright friend")
+    print(fridge)
+
+def foodPicker():
+    food = len(fridge)
+    pick = randint(0, food)
+    print("\n You shall be eating:", fridge[pick])
+
+def runSystem():
+    storeFridge()
+    foodPicker()
+
+#Run the system
